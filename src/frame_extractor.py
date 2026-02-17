e38ff752fbd7a270888b306fcadb5eee3c7e870c
lenyDENOYELLE <150019077+lenyDENOYELLE@users.noreply.github.com>
2026-02-17 18:56:30 -0500

    Create frame_extractor.py
    Added file containing functions for extracting frames from a video file

---
+++ src/frame_extractor.py
@@ -0,0 +1,51 @@
+import cv2
+import os, shutil
+
+def extract_all_frames(video_path : str, folder_for_frames : str):
+    """
+    Extract all frames with the names "frame[n].jpg" with n the number of the frame in the folder specified
+
+    :param video_path: path of the video to extract all the frames
+    :type video_path: str
+    :param folder_for_frames: Path of the folder which will contain all the frames
+    :type folder_for_frames: str
+    """
+
+    if not os.path.isfile(video_path) :
+        print("The file" + video_path + " was not found")
+        exit
+    print("Extracting '" + video_path +"' frames...")
+
+    if not os.path.exists(folder_for_frames):
+        os.makedirs(folder_for_frames)
+        print("Created folder '" + folder_for_frames + "'.")
+
+    capture = cv2.VideoCapture(video_path)
+    i = 1
+
+    while(capture.isOpened):
+        no_more_frames, frame = capture.read()
+        if no_more_frames == False:
+            break
+        frame_name = folder_for_frames + "/frame"+str(i)+".jpg"
+        cv2.imwrite(frame_name, frame)
+        i+=1
+    print ("Total of frames extracted : " + str(i-1))
+
+def delete_all_files_in_folder(folder : str):
+    """
+    Deletes all files in the folder specified
+    (can be used to clear frames in a folder)
+
+    :param folder: name of the folder from which to remove files
+    :type folder: str
+    """
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
